fbfacf971c629634acbed2013528727d14d2bf0f
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 19:08:06 +0000

    feat: add navigation component

---
+++ components/o/navigation.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-navigation-drawer
+    v-model="right"
+    floating
+    :mini-variant.sync="mini"
+    app
+    right
+  >
+    <v-list-item class="px-2">
+      <v-list-item-content>
+        <v-list-item-title> {{ $auth.user.name }}</v-list-item-title>
+        <v-list-item-subtitle> {{ $auth.user.email }} </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list dense nav>
+      <v-list-item
+        v-for="item in navigationitems"
+        :key="item.title"
+        link
+        :to="item.link"
+        color="primary"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'NavigationComp',
+
+  data() {
+    return {
+      navigationitems: [
+        {
+          title: 'بارگذاری سند',
+          link: '/upload',
+          icon: 'mdi-file-document-multiple-outline',
+        },
+        // {
+        //   title: 'بارگذاری سند',
+        //   link: '/upload',
+        //   icon: 'mdi-file-document-multiple-outline',
+        // },
+      ],
+      right: true,
+    }
+  },
+
+  computed: {
+    // use for v-model : mini drawer state
+    mini: {
+      get() {
+        return this.$store.state.mini
+      },
+      set(val) {
+        this.$store.commit('SET_MINI', val)
+      },
+    },
+  },
+
+  methods: {
+    // use in parent component
+    toggleRight() {
+      this.right = !this.right
+    },
+  },
+}
+</script>
+
+<style scoped></style>
