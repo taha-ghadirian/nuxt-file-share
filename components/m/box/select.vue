c9bb8c64d791ccf286a30f3e432f5f0fffac43d5
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 10:51:22 +0000

    feat: add select box component

---
+++ components/m/box/select.vue
@@ -0,0 +1,60 @@
+<template>
+  <validation-provider
+    v-slot="{ errors }"
+    :mode="validateMode"
+    :name="label"
+    :rules="rules"
+  >
+    <v-select
+      :class="{ 'v-messages__message__ltr': dir === 'ltr' }"
+      :dir="dir ? dir : 'rtl'"
+      :error-messages="errors"
+      :label="label"
+      v-bind="$attrs"
+      v-on="$listeners"
+  
+      @input="$emit('input', $event.value)"
+      
+    />
+  </validation-provider>
+</template>
+
+<script>
+import { ValidationProvider } from 'vee-validate'
+
+export default {
+  name: 'SelectBoxMolecule',
+  components: {
+    ValidationProvider,
+  },
+  inheritAttrs: false,
+  props: {
+    validateMode: {
+      type: String,
+      default: 'eager',
+    },
+    dir: {
+      type: String,
+      default: 'rtl',
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    rules: {
+      type: [String, () => Object],
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+.v-messages__message__ltr .v-messages {
+  text-align: left !important;
+}
+
+.v-messages__message__ltr .error--text {
+  text-align: right !important;
+}
+</style>
