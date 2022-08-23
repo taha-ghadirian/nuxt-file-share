ecfbc31a81979d8846e1b24d2b2fa2bc3d83cf02
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 06:53:35 +0000

    chore: add overlay component

---
+++ components/o/auth/overlay.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-col
+    cols="12"
+    class="fill-height pa-0 light d-none d-md-flex flex-column justify-center align-center"
+    md="7"
+  >
+    <v-img src="/imgs/login.svg" max-width="400" max-height="400" contain />
+    <h1 class="text-h4 mb-2 primary--text">File Sharing</h1>
+    <p class="text-body-1">
+      File Sharing یک وب سایت برای به اشتراک گذاری مدارک شما
+    </p>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'OverviewAuthOrganism',
+}
+</script>
+
+<style></style>
