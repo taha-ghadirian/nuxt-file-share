2e8328d9c7c12c7dcc8cceaa021ba0c008891196
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 19:15:26 +0000

    feat: add footer component

---
+++ components/o/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer inset color="dark" app>
+    <v-col class="text-center">
+      {{ new Date().getFullYear() }} — <strong>File Shering</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterOrganism',
+}
+</script>
+
+<style scoped></style>
