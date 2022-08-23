b4a77c6ea142bccf2b502daebb6cd2437a7394e6
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 19:07:14 +0000

    feat: add dark-mode-switcher component

---
+++ components/m/dark-mode-switcher.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ml-4">
+    <label for="DarkModeSwitcher">
+      <v-icon v-show="!$vuetify.theme.dark"> mdi-white-balance-sunny</v-icon>
+      <v-icon v-show="$vuetify.theme.dark"> mdi-weather-night</v-icon>
+    </label>
+    <input
+      id="DarkModeSwitcher"
+      v-model="$vuetify.theme.dark"
+      type="checkbox"
+      class="d-none"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DarkModeSwitcher',
+}
+</script>
+
+<style scoped></style>
