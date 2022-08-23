1b5c9a5a53213b5a63c84f67a2cf8ebdff386d86
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 05:09:38 +0000

    feat: add form box component

---
+++ components/m/box/form.vue
@@ -0,0 +1,29 @@
+<template>
+  <ValidationObserver v-slot="{ invalid, handleSubmit }">
+    <form
+      v-bind="$attrs"
+      v-on="$listeners"
+      @submit.prevent="handleSubmit(submit)"
+    >
+      <slot :invalid="invalid"></slot>
+    </form>
+  </ValidationObserver>
+</template>
+
+<script>
+import { ValidationObserver } from 'vee-validate'
+
+export default {
+  name: 'AppForm',
+  components: {
+    ValidationObserver,
+  },
+  inheritAttrs: false,
+  props: {
+    submit: {
+      required: true,
+      type: Function,
+    },
+  },
+}
+</script>
