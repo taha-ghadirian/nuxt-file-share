49dbfec6e079bc8e5591156085993c4b8ec57dde
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 19:21:16 +0000

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <O-Navigation ref="navbar" />
+
+    <O-Navbar @toggle-drawer="$refs.navbar.toggleRight()" />
+
+    <v-main class="light">
+      <!-- page station -->
+      <Nuxt />
+    </v-main>
+
+    <O-Footer />
+    <M-Snackbar></M-Snackbar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style></style>
