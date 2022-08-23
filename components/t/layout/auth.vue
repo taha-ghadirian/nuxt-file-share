81770e0406b19a70190e512f2a84aee5d5008630
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 06:58:03 +0000

    feat: add auth view

---
+++ components/t/layout/auth.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid class="fill-height py-0">
+        <v-row class="fill-height">
+          <!-- 
+            dynamic section 
+            auth forms 
+            right section in desktop size 
+          -->
+          <v-col
+            cols="12"
+            md="5"
+            class="fill-height d-flex align-center justify-center"
+          >
+            <Nuxt></Nuxt>
+          </v-col>
+          <!-- end dynamic section-->
+
+          <!--
+              static component
+              image section
+              left section in desktop size
+           -->
+          <O-AuthOverlay></O-AuthOverlay>
+          <!-- end static section-->
+        </v-row>
+      </v-container>
+    </v-main>
+    <!-- this component regesterd in all layout level -->
+    <M-Snackbar></M-Snackbar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'AuthLayoutTepmlate',
+}
+</script>
+
+<style></style>
---
+++ layouts/auth.vue
@@ -0,0 +1,11 @@
+<template>
+  <T-LayoutAuth></T-LayoutAuth>
+</template>
+
+<script>
+export default {
+  name: 'AuthLayout',
+}
+</script>
+
+<style scoped></style>
