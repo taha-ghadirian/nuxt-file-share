92860e5dfb791e628744fa87c65fa307d2ea94a4
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-23 06:58:26 +0000

    feat: add login view

---
+++ components/o/auth/form/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <M-BoxForm :submit="submitLoginForm">
+    <M-BoxInput
+      v-model="username"
+      rules="required|min:5|max:100"
+      label="نام کاربری"
+      outlined
+    ></M-BoxInput>
+    <M-BoxInput
+      v-model="password"
+      rules="required|min:8|max:100"
+      type="password"
+      label="رمزعبور"
+      outlined
+    ></M-BoxInput>
+    <v-btn type="submit" block color="primary"> ورود </v-btn>
+  </M-BoxForm>
+</template>
+
+<script>
+export default {
+  name: 'LoginFormOrganism',
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitLoginForm() {
+      alert('loggedIn')
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="login-form-container">
+    <nuxt-link
+      to="/"
+      class="text-h6 text-center d-block text-decoration-none primary--text"
+    >
+      File Sharing
+    </nuxt-link>
+    <p class="text-subtitle-1 text-center">
+      برای ورود به File Sharing اطلاعات زیر را وارد کنید
+    </p>
+    <O-AuthFormLogin></O-AuthFormLogin>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  layout: 'auth',
+}
+</script>
+
+<style></style>
